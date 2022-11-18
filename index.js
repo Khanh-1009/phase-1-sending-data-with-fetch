6241970ec0e9a80246e6ddaf05bb552ba17488c4
Khanh Tran <katetrannbkhanh@gmail.com>
2022-11-17 23:47:42 -0800

    commit tree 61b0da207261b1809eb2ac087e66ccfc08396c8a

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+function submitData(userName,userEmail){
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers: {
+            'Content-Type':"application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(resp => resp.json())
+    .then(user => document.body.innerHTML = user.id)
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
